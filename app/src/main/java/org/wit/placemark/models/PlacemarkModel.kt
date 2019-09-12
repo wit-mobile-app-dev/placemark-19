51769bc8831bff9a7dd40b49608779262d7e1556
Eamonn de Leastar <edeleastar@wit.ie>
2019-09-12 16:51:07 +0100

    include image path in Placemark

--- app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
+++ app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
@@ -6,5 +6,6 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class PlacemarkModel(var id: Long = 0,
                           var title: String = "",
-                          var description: String = "") : Parcelable
+                          var description: String = "",
+                          var image: String = "") : Parcelable
 
