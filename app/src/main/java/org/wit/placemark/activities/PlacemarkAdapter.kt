365db10003cb352c9bdc8a3cf757fbcbf517a77a
Eamonn de Leastar <edeleastar@wit.ie>
2019-09-12 16:25:33 +0100

    incorporate PlacemarkListener in the adapter

--- app/src/main/java/org/wit/placemark/activities/PlacemarkAdapter.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkAdapter.kt
@@ -8,7 +8,12 @@ import kotlinx.android.synthetic.main.card_placemark.view.*
 import org.wit.placemark.R
 import org.wit.placemark.models.PlacemarkModel
 
-class PlacemarkAdapter constructor(private var placemarks: List<PlacemarkModel>) : RecyclerView.Adapter<PlacemarkAdapter.MainHolder>() {
+interface PlacemarkListener {
+  fun onPlacemarkClick(placemark: PlacemarkModel)
+}
+
+class PlacemarkAdapter constructor(private var placemarks: List<PlacemarkModel>,
+                                   private val listener: PlacemarkListener) : RecyclerView.Adapter<PlacemarkAdapter.MainHolder>() {
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
     return MainHolder(LayoutInflater.from(parent?.context).inflate(R.layout.card_placemark, parent, false))
@@ -16,16 +21,17 @@ class PlacemarkAdapter constructor(private var placemarks: List<PlacemarkModel>)
 
   override fun onBindViewHolder(holder: MainHolder, position: Int) {
     val placemark = placemarks[holder.adapterPosition]
-    holder.bind(placemark)
+    holder.bind(placemark, listener)
   }
 
   override fun getItemCount(): Int = placemarks.size
 
   class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    fun bind(placemark: PlacemarkModel) {
+    fun bind(placemark: PlacemarkModel, listener: PlacemarkListener) {
       itemView.placemarkTitle.text = placemark.title
       itemView.description.text = placemark.description
+      itemView.setOnClickListener { listener.onPlacemarkClick(placemark) }
     }
   }
 }
