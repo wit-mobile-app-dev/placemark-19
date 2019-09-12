9d81bd63a00d423e116cebfc340d435ab9cfbb0f
Eamonn de Leastar <edeleastar@wit.ie>
2019-09-12 17:09:37 +0100

    display image on card

--- app/src/main/java/org/wit/placemark/activities/PlacemarkAdapter.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.card_placemark.view.*
 import org.wit.placemark.R
+import org.wit.placemark.helpers.readImageFromPath
 import org.wit.placemark.models.PlacemarkModel
 
 interface PlacemarkListener {
@@ -31,6 +32,7 @@ class PlacemarkAdapter constructor(private var placemarks: List<PlacemarkModel>,
     fun bind(placemark: PlacemarkModel, listener: PlacemarkListener) {
       itemView.placemarkTitle.text = placemark.title
       itemView.description.text = placemark.description
+      itemView.imageIcon.setImageBitmap(readImageFromPath(itemView.context, placemark.image))
       itemView.setOnClickListener { listener.onPlacemarkClick(placemark) }
     }
   }
