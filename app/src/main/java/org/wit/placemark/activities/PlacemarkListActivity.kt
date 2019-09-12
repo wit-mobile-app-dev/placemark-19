c3e67eab882134eca7863d1433632ed6a4e948b4
Eamonn de Leastar <edeleastar@wit.ie>
2019-09-12 16:37:46 +0100

    update placemark on return to PlacemarkListActivity

--- app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
@@ -1,5 +1,6 @@
 package org.wit.placemark.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
@@ -42,6 +43,11 @@ class PlacemarkListActivity : AppCompatActivity(), PlacemarkListener {
   }
 
   override fun onPlacemarkClick(placemark: PlacemarkModel) {
-    startActivityForResult(intentFor<PlacemarkActivity>().putExtra("placemark_edit", placemark), AppCompatActivity.RESULT_OK)
+    startActivityForResult(intentFor<PlacemarkActivity>().putExtra("placemark_edit", placemark), 0)
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    recyclerView.adapter?.notifyDataSetChanged()
+    super.onActivityResult(requestCode, resultCode, data)
   }
 }
