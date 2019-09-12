befb0fa4a6ceca43f7c6904b96b7430ccd9c7299
Eamonn de Leastar <edeleastar@wit.ie>
2019-09-12 14:16:01 +0100

    refactor to to PlacemarkActivity

--- app/src/main/java/org/wit/placemark/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/PlacemarkActivity.kt
@@ -3,10 +3,10 @@ package org.wit.placemark
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class PlacemarkActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(R.layout.activity_placemark)
   }
 }
