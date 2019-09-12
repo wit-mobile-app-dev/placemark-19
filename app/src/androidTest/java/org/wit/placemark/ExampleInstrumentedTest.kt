dd6ddf50451528fcdd01d93d762c02025d46c16f
Eamonn de Leastar <edeleastar@wit.ie>
2019-09-12 14:13:54 +0100

    reformat all code to 2 space indent

--- app/src/androidTest/java/org/wit/placemark/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/org/wit/placemark/ExampleInstrumentedTest.kt
@@ -1,7 +1,8 @@
 package org.wit.placemark
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
+//import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,10 +16,10 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("org.wit.placemark", appContext.packageName)
-    }
+  @Test
+  fun useAppContext() {
+    // Context of the app under test.
+    val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+    assertEquals("org.wit.placemark", appContext.packageName)
+  }
 }
