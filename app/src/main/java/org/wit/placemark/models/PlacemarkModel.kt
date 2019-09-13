c5db84aed300ec7ed415f296f1e5cb17a455337e
Eamonn de Leastar <edeleastar@wit.ie>
2019-09-13 06:46:30 +0100

    Location Model introduced

--- app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
+++ app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
@@ -9,3 +9,9 @@ data class PlacemarkModel(var id: Long = 0,
                           var description: String = "",
                           var image: String = "") : Parcelable
 
+@Parcelize
+data class Location(var lat: Double = 0.0,
+                    var lng: Double = 0.0,
+                    var zoom: Float = 0f) : Parcelable
+
+
