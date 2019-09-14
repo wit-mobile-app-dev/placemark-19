c08127c320299de4c65aa10432416dee81f09f0f
Eamonn de Leastar <edeleastar@wit.ie>
2019-09-14 16:25:03 +0100

    new PlacemarkStore implementation

---
+++ app/src/main/java/org/wit/placemark/models/PlacemarkJSONStore.kt
@@ -0,0 +1,64 @@
+package org.wit.placemark.models
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import org.jetbrains.anko.AnkoLogger
+import org.wit.placemark.helpers.*
+import java.util.*
+
+val JSON_FILE = "placemarks.json"
+val gsonBuilder = GsonBuilder().setPrettyPrinting().create()
+val listType = object : TypeToken<java.util.ArrayList<PlacemarkModel>>() {}.type
+
+fun generateRandomId(): Long {
+  return Random().nextLong()
+}
+
+class PlacemarkJSONStore : PlacemarkStore, AnkoLogger {
+
+  val context: Context
+  var placemarks = mutableListOf<PlacemarkModel>()
+
+  constructor (context: Context) {
+    this.context = context
+    if (exists(context, JSON_FILE)) {
+      deserialize()
+    }
+  }
+
+  override fun findAll(): MutableList<PlacemarkModel> {
+    return placemarks
+  }
+
+  override fun create(placemark: PlacemarkModel) {
+    placemark.id = generateRandomId()
+    placemarks.add(placemark)
+    serialize()
+  }
+
+  override fun update(placemark: PlacemarkModel) {
+    val placemarksList = findAll() as ArrayList<PlacemarkModel>
+    var foundPlacemark: PlacemarkModel? = placemarksList.find { p -> p.id == placemark.id }
+    if (foundPlacemark != null) {
+      foundPlacemark.title = placemark.title
+      foundPlacemark.description = placemark.description
+      foundPlacemark.image = placemark.image
+      foundPlacemark.lat = placemark.lat
+      foundPlacemark.lng = placemark.lng
+      foundPlacemark.zoom = placemark.zoom
+    }
+    serialize()
+  }
+
+  private fun serialize() {
+    val jsonString = gsonBuilder.toJson(placemarks, listType)
+    write(context, JSON_FILE, jsonString)
+  }
+
+  private fun deserialize() {
+    val jsonString = read(context, JSON_FILE)
+    placemarks = Gson().fromJson(jsonString, listType)
+  }
+}
