c742e8b3ac86f0c5a323a46eaf564b25f22c4b87
Eamonn de Leastar <edeleastar@wit.ie>
2019-09-14 16:26:03 +0100

    switch to using PlacemarkJSONStore

--- app/src/main/java/org/wit/placemark/main/MainApp.kt
+++ app/src/main/java/org/wit/placemark/main/MainApp.kt
@@ -3,14 +3,17 @@ package org.wit.placemark.main
 import android.app.Application
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.wit.placemark.models.PlacemarkJSONStore
 import org.wit.placemark.models.PlacemarkMemStore
+import org.wit.placemark.models.PlacemarkStore
 
 class MainApp : Application(), AnkoLogger {
 
-  val placemarks = PlacemarkMemStore()
+  lateinit var placemarks: PlacemarkStore
 
   override fun onCreate() {
     super.onCreate()
+    placemarks = PlacemarkJSONStore(applicationContext)
     info("Placemark started")
   }
 }
