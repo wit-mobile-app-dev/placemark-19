df729ca41724a767a082631ca932881f9b211d54
Eamonn de Leastar <edeleastar@wit.ie>
2019-10-07 09:18:20 +0100

    switch to PlacemarkStoreRoom

--- app/src/main/java/org/wit/placemark/main/MainApp.kt
+++ app/src/main/java/org/wit/placemark/main/MainApp.kt
@@ -3,8 +3,8 @@ package org.wit.placemark.main
 import android.app.Application
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
-import org.wit.placemark.models.json.PlacemarkJSONStore
 import org.wit.placemark.models.PlacemarkStore
+import org.wit.placemark.room.PlacemarkStoreRoom
 
 class MainApp : Application(), AnkoLogger {
 
@@ -12,7 +12,7 @@ class MainApp : Application(), AnkoLogger {
 
   override fun onCreate() {
     super.onCreate()
-    placemarks = PlacemarkJSONStore(applicationContext)
+    placemarks = PlacemarkStoreRoom(applicationContext)
     info("Placemark started")
   }
 }
