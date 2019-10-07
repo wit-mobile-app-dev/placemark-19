d99340949d59d42a84fd9bd332017124659356d5
Eamonn de Leastar <edeleastar@wit.ie>
2019-10-07 12:23:55 +0100

    engage firebaseAuth service

--- app/src/main/java/org/wit/placemark/views/login/LoginPresenter.kt
+++ app/src/main/java/org/wit/placemark/views/login/LoginPresenter.kt
@@ -1,16 +1,36 @@
 package org.wit.placemark.views.login
 
+import com.google.firebase.auth.FirebaseAuth
+import org.jetbrains.anko.toast
 import org.wit.placemark.views.BasePresenter
 import org.wit.placemark.views.BaseView
 import org.wit.placemark.views.VIEW
 
 class LoginPresenter(view: BaseView) : BasePresenter(view) {
 
+  var auth: FirebaseAuth = FirebaseAuth.getInstance()
+
   fun doLogin(email: String, password: String) {
-    view?.navigateTo(VIEW.LIST)
+    view?.showProgress()
+    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(view!!) { task ->
+      if (task.isSuccessful) {
+        view?.navigateTo(VIEW.LIST)
+      } else {
+        view?.toast("Sign Up Failed: ${task.exception?.message}")
+      }
+      view?.hideProgress()
+    }
   }
 
   fun doSignUp(email: String, password: String) {
-    view?.navigateTo(VIEW.LIST)
+    view?.showProgress()
+    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(view!!) { task ->
+      if (task.isSuccessful) {
+        view?.navigateTo(VIEW.LIST)
+      } else {
+        view?.toast("Sign Up Failed: ${task.exception?.message}")
+      }
+      view?.hideProgress()
+    }
   }
 }
