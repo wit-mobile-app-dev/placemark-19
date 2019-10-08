948d7b73c2c3a9f9a4145f1fe359407856955829
Eamonn de Leastar <edeleastar@wit.ie>
2019-10-08 07:17:26 +0100

    adjust Login to fetch firebase database when logging in

--- app/src/main/java/org/wit/placemark/views/login/LoginPresenter.kt
+++ app/src/main/java/org/wit/placemark/views/login/LoginPresenter.kt
@@ -2,6 +2,7 @@ package org.wit.placemark.views.login
 
 import com.google.firebase.auth.FirebaseAuth
 import org.jetbrains.anko.toast
+import org.wit.placemark.models.firebase.PlacemarkFireStore
 import org.wit.placemark.views.BasePresenter
 import org.wit.placemark.views.BaseView
 import org.wit.placemark.views.VIEW
@@ -9,16 +10,31 @@ import org.wit.placemark.views.VIEW
 class LoginPresenter(view: BaseView) : BasePresenter(view) {
 
   var auth: FirebaseAuth = FirebaseAuth.getInstance()
+  var fireStore: PlacemarkFireStore? = null
+
+  init {
+    if (app.placemarks is PlacemarkFireStore) {
+      fireStore = app.placemarks as PlacemarkFireStore
+    }
+  }
 
   fun doLogin(email: String, password: String) {
     view?.showProgress()
     auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(view!!) { task ->
       if (task.isSuccessful) {
-        view?.navigateTo(VIEW.LIST)
+        if (fireStore != null) {
+          fireStore!!.fetchPlacemarks {
+            view?.hideProgress()
+            view?.navigateTo(VIEW.LIST)
+          }
+        } else {
+          view?.hideProgress()
+          view?.navigateTo(VIEW.LIST)
+        }
       } else {
+        view?.hideProgress()
         view?.toast("Sign Up Failed: ${task.exception?.message}")
       }
-      view?.hideProgress()
     }
   }
 
@@ -26,11 +42,12 @@ class LoginPresenter(view: BaseView) : BasePresenter(view) {
     view?.showProgress()
     auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(view!!) { task ->
       if (task.isSuccessful) {
+        view?.hideProgress()
         view?.navigateTo(VIEW.LIST)
       } else {
+        view?.hideProgress()
         view?.toast("Sign Up Failed: ${task.exception?.message}")
       }
-      view?.hideProgress()
     }
   }
 }
