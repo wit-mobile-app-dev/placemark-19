27ab5881098f814395fbb838dbd36f4ffa792d3f
Eamonn de Leastar <edeleastar@wit.ie>
2019-10-08 07:46:19 +0100

    incorporate image upload into FirebaseStorage

--- app/src/main/java/org/wit/placemark/models/firebase/PlacemarkFireStore.kt
+++ app/src/main/java/org/wit/placemark/models/firebase/PlacemarkFireStore.kt
@@ -1,37 +1,45 @@
 package org.wit.placemark.models.firebase
 
 import android.content.Context
+import android.graphics.Bitmap
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import org.jetbrains.anko.AnkoLogger
+import org.wit.placemark.helpers.readImageFromPath
 import org.wit.placemark.models.PlacemarkModel
 import org.wit.placemark.models.PlacemarkStore
+import java.io.ByteArrayOutputStream
+import java.io.File
 
 class PlacemarkFireStore(val context: Context) : PlacemarkStore, AnkoLogger {
 
   val placemarks = ArrayList<PlacemarkModel>()
   lateinit var userId: String
   lateinit var db: DatabaseReference
+  lateinit var st: StorageReference
 
-   override fun findAll(): List<PlacemarkModel> {
+  override fun findAll(): List<PlacemarkModel> {
     return placemarks
   }
 
-   override fun findById(id: Long): PlacemarkModel? {
+  override fun findById(id: Long): PlacemarkModel? {
     val foundPlacemark: PlacemarkModel? = placemarks.find { p -> p.id == id }
     return foundPlacemark
   }
 
-   override fun create(placemark: PlacemarkModel) {
+  override fun create(placemark: PlacemarkModel) {
     val key = db.child("users").child(userId).child("placemarks").push().key
     key?.let {
       placemark.fbId = key
       placemarks.add(placemark)
       db.child("users").child(userId).child("placemarks").child(key).setValue(placemark)
+      updateImage(placemark)
     }
   }
 
-   override fun update(placemark: PlacemarkModel) {
+  override fun update(placemark: PlacemarkModel) {
     var foundPlacemark: PlacemarkModel? = placemarks.find { p -> p.fbId == placemark.fbId }
     if (foundPlacemark != null) {
       foundPlacemark.title = placemark.title
@@ -41,16 +49,43 @@ class PlacemarkFireStore(val context: Context) : PlacemarkStore, AnkoLogger {
     }
 
     db.child("users").child(userId).child("placemarks").child(placemark.fbId).setValue(placemark)
-
+    if ((placemark.image.length) > 0 && (placemark.image[0] != 'h')) {
+      updateImage(placemark)
+    }
   }
 
-   override fun delete(placemark: PlacemarkModel) {
+  override fun delete(placemark: PlacemarkModel) {
     db.child("users").child(userId).child("placemarks").child(placemark.fbId).removeValue()
     placemarks.remove(placemark)
   }
 
   override fun clear() {
     placemarks.clear()
+  }
+
+  fun updateImage(placemark: PlacemarkModel) {
+    if (placemark.image != "") {
+      val fileName = File(placemark.image)
+      val imageName = fileName.getName()
+
+      var imageRef = st.child(userId + '/' + imageName)
+      val baos = ByteArrayOutputStream()
+      val bitmap = readImageFromPath(context, placemark.image)
+
+      bitmap?.let {
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+        val uploadTask = imageRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+          println(it.message)
+        }.addOnSuccessListener { taskSnapshot ->
+          taskSnapshot.metadata!!.reference!!.downloadUrl.addOnSuccessListener {
+            placemark.image = it.toString()
+            db.child("users").child(userId).child("placemarks").child(placemark.fbId).setValue(placemark)
+          }
+        }
+      }
+    }
   }
 
   fun fetchPlacemarks(placemarksReady: () -> Unit) {
@@ -64,6 +99,7 @@ class PlacemarkFireStore(val context: Context) : PlacemarkStore, AnkoLogger {
     }
     userId = FirebaseAuth.getInstance().currentUser!!.uid
     db = FirebaseDatabase.getInstance().reference
+    st = FirebaseStorage.getInstance().reference
     placemarks.clear()
     db.child("users").child(userId).child("placemarks").addListenerForSingleValueEvent(valueEventListener)
   }
